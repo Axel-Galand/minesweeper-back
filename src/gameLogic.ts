9e650d511c2c04b2b498a14ef8813b309592ea14
Marcus Belmont <marcus.belmont@etu.unilim.fr>
2022-06-02 10:23:55 +0200

    oui

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -1,11 +1,12 @@
+import Netcode from './Netcode'
 export default class GameLogic {
     greeting: string;
-
+    netc: Netcode;
     constructor(message: string) {
         this.greeting = message;
+
+
     }
 
-    greet() {
-        return this.greeting;
-    }
+    processInput()
 }
